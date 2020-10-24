26f07a97c1d85cb4a25daa90cb4a568b33aa34bf
Anshul Kashyap <akashyap@codifyd.com>
2020-10-24 22:50:26 +0530

    jobs feedack changes

--- src/app/partials/popups/recruiter/show-applicants/show-applicants.component.ts
+++ src/app/partials/popups/recruiter/show-applicants/show-applicants.component.ts
@@ -9,19 +9,60 @@ import { SharedService } from '@app/services/shared.service';
   styleUrls: ['./show-applicants.component.scss'],
 })
 export class ShowApplicantsComponent implements OnInit {
+  uds: any;
   popupData: any;
   applicantJobStatus = applicantJobStatus;
+  bulkStatus = '';
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<ShowApplicantsComponent>,
     public sharedService: SharedService
   ) {
+    this.uds = this.sharedService.plugins.undSco;
     this.popupData = JSON.parse(JSON.stringify(data));
   }
 
-  editApplicantJob() {
+  selDeAll(_type: string) {
+    switch (_type) {
+      case 'select':
+        this.uds.each(this.popupData.job.applicants, (d: any) => {
+          d['isSelected'] = true;
+        });
+        this.sharedService.uiService.showMessage('All Applicants Are Selected');
+        break;
+      case 'deselect':
+        this.uds.each(this.popupData.job.applicants, (d: any) => {
+          d['isSelected'] = false;
+        });
+        this.sharedService.uiService.showMessage('All Applicants Are Deselected');
+        break;
+    }
+  }
+
+  checkDisable() {
+    return this.popupData.job.applicants.length
+      ? this.popupData.job.applicants.filter((d: any) => d.isSelected).length == 0
+      : true;
+  }
+
+  selectApplicant(event: any, applicant: any) {
+    event.stopPropagation();
+    event.preventDefault();
+    applicant['isSelected'] = applicant['isSelected'] ? !applicant['isSelected'] : true;
+  }
+
+  afterStatusChange(_status: string) {
+    this.bulkStatus = _status;
+  }
+
+  editApplicantJob(_bulk?: boolean) {
     const job = this.popupData.job;
     let $t = this;
+    if (_bulk) {
+      this.uds.each(this.popupData.job.applicants, (d: any) => {
+        d['status'] = this.bulkStatus;
+      });
+    }
     $t.sharedService.uiService.showApiStartPopMsg('Updating Applicants Status...');
     let apiUrl = $t.sharedService.urlService.apiCallWithParams('updateJob', { '{jobId}': job.id });
     $t.sharedService.configService.put(apiUrl, job).subscribe(
